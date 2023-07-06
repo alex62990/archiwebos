9620c28cdd651c609123809138f136c58d57b40d
Alexandre Sailly <alexandresailly@icloud.com>
2023-07-06 22:37:20 +0200

    modale effectuée, works à importer

--- Portfolio-architecte-sophie-bluel/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/index.js
@@ -1,152 +1,166 @@
-const gallery = document.querySelector(".gallery");
-const filtres = document.querySelectorAll(".filtre");
+const gallery = document.querySelector(".gallery")
+const filtres = document.querySelectorAll(".filtre")
 const header = document.querySelector(".header")
 const headerNav = document.querySelector(".header-nav")
-const login = document.getElementById("connexion");
-const titreModify = document.querySelector(".titre-modify")
-const imgModify = document.querySelector(".img-modify")
-const containerFiltre = document.querySelector('.container-filtre');
+const login = document.getElementById("connexion")
+const containerFiltre = document.querySelector('.container-filtre')
+const modal = document.querySelector(".modal")
+const modalWrapper = document.querySelector(".modal-wrapper")
 
 function importWorks() {
     fetch("http://localhost:5678/api/works")
         .then(reponse => reponse.json())
         .then(data => {
             works = data;
-            genererWorks(works);
-            displayModal(works)      
+            genererWorks(works)   
+            displayModal(works)   
         });
 }
-importWorks();
+importWorks()
 
 function genererWorks(works) {
-    gallery.innerHTML = "";
+    gallery.innerHTML = ""
 
     works.forEach((work) => {
-        const figure = document.createElement("figure");
-        gallery.appendChild(figure);
+        const figure = document.createElement("figure")
+        gallery.appendChild(figure)
         figure.classList = work.category.name
         figure.setAttribute("data-id", work.id)
 
-        const img = document.createElement("img");
-        img.src = work.imageUrl;
-        figure.appendChild(img);
+        const img = document.createElement("img")
+        img.src = work.imageUrl
+        figure.appendChild(img)
 
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerHTML = work.title;
-        figure.appendChild(figcaption);
-    });
+        const figcaption = document.createElement("figcaption")
+        figcaption.innerHTML = work.title
+        figure.appendChild(figcaption)
+    })
 }
 
 function filtrerWorks() {
     filtres.forEach(filtre => {
-        const valeurFiltre = filtre.textContent;
+        const valeurFiltre = filtre.textContent
 
         filtre.addEventListener("click", () => {
             if (valeurFiltre === "Tous") { worksFiltre = works }
             else { worksFiltre = works.filter((work) => valeurFiltre === work.category.name)}
-            genererWorks(worksFiltre);
-        });
+            genererWorks(worksFiltre)
+        })
     }) 
 }
-filtrerWorks();
+filtrerWorks()
 
-const boutons = document.querySelectorAll('.filtre');
-let bouton = document.querySelector('.filtre');
+const boutons = document.querySelectorAll('.filtre')
+let bouton = document.querySelector('.filtre')
 bouton.classList.add('filtre_selected')
 
 boutons.forEach((bouton) => {                                       // pour chaque boutons filtre, ecoute du clic avec fonction pour chaque bouton filtre
     bouton.addEventListener("click", () => {                        // enlever filtre selectionné pour tous et en meme temps selectionner le filtre target
       boutons.forEach((bouton) => {
-        bouton.classList.remove('filtre_selected');
-      });
-      bouton.classList.add('filtre_selected');
-    });
-  });
+        bouton.classList.remove('filtre_selected')
+      })
+      bouton.classList.add('filtre_selected')
+    })
+  })
 
+// connexion utilisateur
+const modalButton = document.querySelector(".modal-button")
 
-// quand utilisateur se connecte
-
-let token = localStorage.getItem("Token");
-if (token) {
-    const editingBanner = document.createElement("div")
-    header.prepend(editingBanner)
-    editingBanner.classList = "editing-banner"
-    editingBanner.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>
-                               <p class="text-edition">Mode édition</p>
-                               <button class="changement">publier les changements</button>`
-
-    imgModify.innerHTML = `<img src="./assets/images/sophie-bluel.png" alt="">
-			                <a class="modify-image">
-				                <i class="fa-regular fa-pen-to-square"></i>
-				                <p>modifier</p>
-			                </a>`
-
-   titreModify.innerHTML = `<h2>Mes Projets</h2>
-                            <a href="#modal1" class="modify js-modal">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                                modifier
-                            </a>
-                            <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-labelledby="titlemodal" style="display:none;">
-				                <div class="modal-wrapper js-modal-stop">
-					                <button class="js-modal-close btn-xmark"><i class="fa-solid fa-xmark fa-xl"></i></button>
-					                <h3 id="tiltemodal">Galerie photo</h3>
-					                <div class="gallery-modal"></div>
-					                <input type="submit" value="Ajouter une photo">
-					                <a href="#" class="sup-gallery">Supprimer la gallerie</a>
-				                </div>
-			                </aside>`
-
-    containerFiltre.style.display = "none";                 
-    login.innerHTML = "logout"                         // login en logout pour deconnexion
-    login.addEventListener("click", () => {            // ecouter clic sur login devenu logout pour enlever token et revenir sur page de connexion
+  let token = localStorage.getItem("Token")
+  if (token) {
+      const editingBanner = document.createElement("div")
+      header.prepend(editingBanner)
+      editingBanner.classList = "editing-banner"
+      editingBanner.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>
+                                 <p class="text-edition">Mode édition</p>
+                                 <button class="changement">publier les changements</button>`
+    containerFiltre.style.display = "none"               
+    login.innerHTML = "logout" 
+    modalButton.style.display = "flex"
+    login.addEventListener("click", () => {            
         localStorage.removeItem("Token")
         window.location.replace("login.html")
-    })
-};
+    }) 
+}
 
-// apparation fenetre modale
-let modal = null
+// modale utilisateur
 
-const openModal = function (e) {
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute('href'))
-    target.style.display = null
-    target.removeAttribute('aria-hidden')
-    target.setAttribute('aria-modal', 'true')
-    modal = target
+document.querySelectorAll(".modal-button").forEach(btn => {
+    btn.addEventListener("click", () => openModalCloseModal())
+})
+
+function openModalCloseModal() {
+    modal.style.display = "flex"
+    modalWrapper.innerHTML = `
+                                <button class="js-modal-close btn-xmark"><i class="fa-solid fa-xmark fa-xl"></i></button>
+                                <h3 id="tiltemodal">Galerie photo</h3>
+                                <div class="gallery-modal"></div>
+                                <button class="js-modal btn-modal2">Ajouter une photo</button>
+                                <button class="sup-gallery">Supprimer la gallerie</button>
+                            `
     modal.addEventListener('click', closeModal)
     modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
     modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
-}
 
-const closeModal = function (e) {
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = 'none'
-    modal.setAttribute('aria-hidden', 'true')
-    modal.removeAttribute('aria-modal')
-    modal.removeEventListener('click', closeModal)
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
-    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
-    modal = null
+
+    const modalClose = document.querySelector(".js-modal-close")
+    modalClose.addEventListener("click", () => closeModal())
+
+        function closeModal() {
+            modal.style.display = "none"
+            modal.removeEventListener('click', closeModal)
+            modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+            modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+        }
+        
+        window.addEventListener('keydown', function (e) {
+            if (e.key === "Escape" || e.key === "Esc") {
+                closeModal(e)
+            }
+        })
+
+                const btnModal2 = document.querySelector(".btn-modal2")
+                btnModal2.addEventListener("click", () => openModal2())
+
+                function openModal2() {
+                    modalWrapper.innerHTML = `
+                                    <button class="js-modal-return btn-arrow-left"><i class="fa-solid fa-arrow-left fa-xl"></i></button>
+                                    <button class="js-modal-close btn-xmark"><i class="fa-solid fa-xmark fa-xl"></i></button>
+                                    <h3 id="tiltemodal">Ajout Photo</h3>
+                                    <div class="ajout-photo">
+                                        <div class="photo">
+                                            <div class="insertion-photo">
+                                                <i class="fa-regular fa-image fa-5x"></i>
+                                            </div>
+                                            <a href="#" class="btn-ajout-photo">+ Ajouter photo</a>
+                                            <p class="dim-photo">jpg, png : 4mo max</p>
+                                        </div>
+                                        <label for="titre">Titre</label>
+			                            <input type="text" name="titre" id="titre">
+                                        <label for="categories">Catégories</label>
+                                        <select name="categorie" id="categories">
+                                            <option value=""></option>
+                                            <option value="Objets">Objets</option>
+                                            <option value="Appartements">Appartements</option>
+                                            <option value="Hotels & restaurants">Hotels & restaurants</option>
+                                    </div>
+                                    <input type="submit" value="Valider">
+
+                    `
+                    const modalClose = document.querySelector(".js-modal-close")
+                    modalClose.addEventListener("click", () => closeModal())
+
+                    const arrowLeft = document.querySelector(".js-modal-return")
+                    arrowLeft.addEventListener("click", () => openModalCloseModal())
+                }
+                
 }
 
 const stopPropagation = function (e) {
     e.stopPropagation()
 }
 
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal)
-})
-
-window.addEventListener('keydown', function (e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeModal(e)
-    }
-})
-
-// recuperation et supp des works pour la modale
-
+// recup des travaux et suppression dans la modale
 const galleryModal = document.querySelector(".gallery-modal")
 
 function displayModal(works) {
@@ -161,7 +175,6 @@ function displayModal(works) {
             </div>    
         `;
     })
-    galleryModal.innerHTML = imageModalHtml;
 
     const suppWork = document.querySelectorAll(".fa-trash-can")
 
@@ -198,6 +211,3 @@ function displayModal(works) {
         })
     })
 }
-
-
-
