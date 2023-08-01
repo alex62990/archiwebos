0e69f8299cf321c9329349c42dc53905d2b80678
Alexandre Sailly <alexandresailly@icloud.com>
2023-08-01 15:20:16 +0200

    modication login html et js avec vérification des inputs

--- Portfolio-architecte-sophie-bluel/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -1,17 +1,28 @@
 let password = document.querySelector('#password')
 let email = document.querySelector('#email')
-let submitFormLogin = document.getElementById("form")
+let submitFormLogin = document.getElementById("formLogin");
 
 
-submitFormLogin.addEventListener("submit", async (event) => {                     // ecoute bouton login avec preventDefault pour attendre les instructions avant de lancer event
-    event.preventDefault()
+submitFormLogin.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-    fetch("http://localhost:5678/api/users/login", {                                // recup fetch login users avec methode post
+    // Expression régulière pour vérifier le format de l'email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email.value)) {
+        // Si l'email n'est pas valide, afficher un message d'erreur ou prendre une autre action
+        const messageAddress = document.querySelector("#message-address");
+        messageAddress.style.display = "flex";
+        return;
+
+    } else {
+
+    fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify({                                                      // verification des input avec .value sur email et password
+        body: JSON.stringify({
             email: email.value,
             password: password.value,
         }),
@@ -19,12 +30,13 @@ submitFormLogin.addEventListener("submit", async (event) => {                   
         .then(reponse => reponse.json())
         .then(data => {
             let token = data.token
-            localStorage.setItem("Token", token)                                  // stocker token dans le localStorage
-            if (token) {                                                            // si token correspond redirection sur index sinon message erreur
-                window.location.replace('index.html')
+            localStorage.setItem("Token", token);
+            if (token) {
+                window.location.replace("index.html");
             } else {
-                const messageLogin = document.querySelector(".message-login")
-                messageLogin.style.display = "flex"
+                const messageLogin = document.querySelector(".message-login");
+                messageLogin.style.display = "flex";
             }
-        })
-})
+        });
+    }
+});
