ab8ce683963958a7a0fa750e3f6afc7347e5c463
Alexandre Sailly <alexandresailly@icloud.com>
2023-08-01 10:38:20 +0200

    modification d'envoie du formulaire

--- Portfolio-architecte-sophie-bluel/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -1,9 +1,9 @@
 let password = document.querySelector('#password')
 let email = document.querySelector('#email')
-let submit= document.querySelector('#submit')
+let submitFormLogin = document.getElementById("form")
 
 
-let boutonLogin = submit.addEventListener("click", (event) => {                     // ecoute bouton login avec preventDefault pour attendre les instructions avant de lancer event
+submitFormLogin.addEventListener("submit", async (event) => {                     // ecoute bouton login avec preventDefault pour attendre les instructions avant de lancer event
     event.preventDefault()
 
     fetch("http://localhost:5678/api/users/login", {                                // recup fetch login users avec methode post
@@ -21,10 +21,10 @@ let boutonLogin = submit.addEventListener("click", (event) => {                 
             let token = data.token
             localStorage.setItem("Token", token)                                  // stocker token dans le localStorage
             if (token) {                                                            // si token correspond redirection sur index sinon message erreur
-                window.location.replace("index.html")
+                window.location.replace('index.html')
             } else {
                 const messageLogin = document.querySelector(".message-login")
                 messageLogin.style.display = "flex"
             }
         })
-    })
+})
